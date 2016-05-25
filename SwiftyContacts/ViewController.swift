bfb694beda206916ba1bfdb69e62f4c8ea8d7279
PatrickCooke <cookepa1@gmail.com>
2016-05-25 13:52:41 -0400

    added ability to sort order between first name and last name

--- SwiftyContacts/ViewController.swift
+++ SwiftyContacts/ViewController.swift
@@ -11,7 +11,7 @@ import CoreData
 import Contacts
 import ContactsUI
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CNContactPickerDelegate, CNContactViewControllerDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate { //CNContactPickerDelegate, CNContactViewControllerDelegate {
 
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
@@ -20,42 +20,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet private weak var lastNameTxtField:UITextField!
     var contactStore = CNContactStore()
     var indexArray = [String]()
+    var sortOrder = true
 
-    //MARK: - ContactPicker Methods
-    
-    @IBAction private func showContactList(sender: UIBarButtonItem){
-        print("Show Contact List")
-        let contactListVC = CNContactPickerViewController()
-        contactListVC.delegate = self
-        presentViewController(contactListVC, animated: true, completion: nil)
-    }
-    
-    func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
-        let entityDescription = NSEntityDescription.entityForName("Contact", inManagedObjectContext: managedObjectContext)!
-        let newlistcontact = Contact(entity: entityDescription, insertIntoManagedObjectContext: managedObjectContext)
-        newlistcontact.lastName = contact.familyName
-        newlistcontact.firstName = contact.givenName
-        if let email = contact.emailAddresses.first?.value as? String {
-            newlistcontact.emailAddress = email
-        }
-        if let address = contact.postalAddresses.first {
-            let addressValue = address.value as! CNPostalAddress
-            newlistcontact.streetAddress = addressValue.street
-            newlistcontact.cityAddress = addressValue.city
-            newlistcontact.stateAddress = addressValue.state
-            newlistcontact.zipAddress = addressValue.postalCode
-        }
-        if let phone = contact.phoneNumbers.first?.value as? CNPhoneNumber {
-            newlistcontact.phoneNumber = phone.stringValue
-        }
-        
-        
-        newlistcontact.rating = 0
-        newlistcontact.contactIdentifer = contact.identifier
-        appDelegate.saveContext()
-    }
     
     //MARK: - Interactivty Methods
+    
+    @IBAction private func switchSortMethod(){
+        sortOrder = !sortOrder
+        refreshTableData()
+    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let destController = segue.destinationViewController as! DetailsViewController
@@ -82,17 +55,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         //return contactArray.count
     }
     
-   /*
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView .dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-//        let currentContact = contactArray[indexPath.row]
-        let sectionArray = filterArrayForSection(contactArray, section: indexPath.section)
-        let currentContact = sectionArray[indexPath.row]
-        cell.textLabel?.text = "\(currentContact.lastName!), \(currentContact.firstName!) "
-        cell.detailTextLabel?.text = "\(currentContact.emailAddress!)"
-        return cell
-    }
-   */
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView .dequeueReusableCellWithIdentifier("Cell2", forIndexPath: indexPath) as! CustomCellTableViewCell
@@ -101,8 +63,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         //cell.nameLabel.text = "\(currentContact.lastName), \(currentContact.firstName)"
         
         if let firstName = currentContact.firstName {
-        if let lastName = currentContact.lastName {
-            cell.nameLabel.text = "\(lastName), \(firstName)"
+            if let lastName = currentContact.lastName {
+                if sortOrder {
+                    cell.nameLabel.text = "\(lastName), \(firstName)"
+                } else {
+                    cell.nameLabel.text = "\(firstName) \(lastName)"
+                }
+                //cell.nameLabel.text = "\(lastName), \(firstName)"
             }
         }
         
@@ -152,7 +119,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     func fetchEntries() -> [Contact]? {
         let fetchRequest = NSFetchRequest(entityName: "Contact")
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "lastName", ascending: true)]
+        if sortOrder {
+            fetchRequest.sortDescriptors = [NSSortDescriptor(key: "lastName", ascending: true)]
+        }else{
+            fetchRequest.sortDescriptors = [NSSortDescriptor(key: "firstName", ascending: true)]
+        }
         do {
             let tempArray = try managedObjectContext.executeFetchRequest(fetchRequest) as! [Contact]
             return tempArray
@@ -163,15 +134,26 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     private func filterArrayForSection(array: [Contact], section: Int) -> [Contact] {
         let sectionHeader = indexArray[section]
-        return array.filter {String($0.lastName![$0.lastName!.startIndex.advancedBy(0)]) == sectionHeader}
+        if sortOrder{
+            return array.filter {String($0.lastName![$0.lastName!.startIndex.advancedBy(0)]) == sectionHeader}
+        } else {
+            return array.filter {String($0.firstName![$0.firstName!.startIndex.advancedBy(0)]) == sectionHeader}
+        }
     }
 
     
     private func createIndexfromArray(array: [Contact]) -> [String] {
-        let letterArray = array.map {String($0.lastName![$0.lastName!.startIndex.advancedBy(0)])}
-        var uniqueArray = Array(Set(letterArray))
-        uniqueArray.sortInPlace()
-        return uniqueArray
+        if sortOrder {
+            let letterArray = array.map {String($0.lastName![$0.lastName!.startIndex.advancedBy(0)])}
+            var uniqueArray = Array(Set(letterArray))
+            uniqueArray.sortInPlace()
+            return uniqueArray
+        } else {
+            let letterArray = array.map {String($0.firstName![$0.firstName!.startIndex.advancedBy(0)])}
+            var uniqueArray = Array(Set(letterArray))
+            uniqueArray.sortInPlace()
+            return uniqueArray
+        }
     }
     
     //MARK: - Contact Access Verification methods
